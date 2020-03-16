d18b926236f5786a708818b46b8a4f07f7720f14
Kalle <kalllebracht@gmx.de>
2020-03-16 16:17:16 +0100

    Y Engine Work!!!!!!

--- main.cpp
+++ main.cpp
@@ -3,7 +3,9 @@
 int main(int argc, char const *argv[]) {
 	MiniGraph mygraph(1.0f, 1.0f);
 	mygraph.add(1.0f,0.0f,0.0f,1.0f);
-	mygraph.add(0.0f,1.0f,0.0f,1.0f);
+	mygraph.add(0.0f,1.0f,0.0f,0.3f);
+	mygraph.add(0.0f,0.0f,1.0f,0.5f);
+	mygraph.add(0.1f,1.0f,1.0f,0.1f);
 
 	mygraph.run();
 
--- minimalgraph.cpp
+++ minimalgraph.cpp
@@ -32,6 +32,8 @@ int MiniGraph::add(float red_channel, float green_channel, float blue_channel, f
 int MiniGraph::run()
 {
 /////////////////INITSTART/////////////////////
+  p_count = p_value.size();
+
 	SDL_Window* window;
 	SDL_Init(SDL_INIT_EVERYTHING);
 
@@ -44,7 +46,7 @@ int MiniGraph::run()
 
 	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 
-	window = SDL_CreateWindow("TEST", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, p_value.size()*100, 400, SDL_WINDOW_OPENGL);
+	window = SDL_CreateWindow("TEST", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, p_count*100, 400, SDL_WINDOW_OPENGL);
 
 	SDL_GLContext glContext = SDL_GL_CreateContext(window);
 
@@ -54,38 +56,35 @@ int MiniGraph::run()
 		cout << "errror";
     return -1;
 	}
-  p_count = p_value.size();
+
 
 /////////////////INITEND///////////////////////
-  float shiftingy = 0.9;
+  float shiftingy = 0.3;
+  float shiftingx = 0;
+
 
   for(int i = 0; i<p_count;i++){
-    float shiftingx = (float)i /(float)10;
-    cout << shiftingx << endl;
-    cout << i << endl;
-    vertices.push_back(Vertex{ 0.2f+shiftingx,-0.9f, 0.0f,              p_red_channel[i], p_green_channel[i], p_blue_channel[i], 0.0f});
-    vertices.push_back(Vertex{ 0.2f+shiftingx, 0.0f+shiftingy, 0.0f,    p_red_channel[i], p_green_channel[i], p_blue_channel[i], 0.0f});
-    vertices.push_back(Vertex{-0.2f+shiftingx,-0.9f, 0.0f,              p_red_channel[i], p_green_channel[i], p_blue_channel[i], 0.0f});
-    vertices.push_back(Vertex{-0.2f+shiftingx, 0.0f+shiftingy, 0.0f,    p_red_channel[i], p_green_channel[i], p_blue_channel[i], 0.0f});
+    float shiftingx = (float)i /(float)2 - (float)0.5f;
+    float shiftingy = (float)p_value[i]*(float)18/(float)10 -(float)0.9f;
+
+     cout << "Y: : "<< shiftingy << endl;
+
+    float use_p_red_channel = p_red_channel[i];
+    float use_p_green_channel = p_green_channel[i];
+    float use_p_blue_channel = p_blue_channel[i];
+
+    vertices.push_back(Vertex{ 0.2f+shiftingx,-0.9f, 0.0f,              use_p_red_channel, use_p_green_channel, use_p_blue_channel, 0.0f});
+    vertices.push_back(Vertex{ 0.2f+shiftingx, 0.0f+shiftingy, 0.0f,    use_p_red_channel, use_p_green_channel, use_p_blue_channel, 0.0f});
+    vertices.push_back(Vertex{-0.2f+shiftingx,-0.9f, 0.0f,              use_p_red_channel, use_p_green_channel, use_p_blue_channel, 0.0f});
+    vertices.push_back(Vertex{-0.2f+shiftingx, 0.0f+shiftingy, 0.0f,    use_p_red_channel, use_p_green_channel, use_p_blue_channel, 0.0f});
   }
-
-/*
-	Vertex vertices[4] = {
-		Vertex{-0.05f+shiftingx,-0.9f, 0.0f,              0.0f, 0.0f, 1.0f, 0.0f}, //Vertex 1
-		Vertex{-0.05f+shiftingx, 0.0f+shiftingy, 0.0f,    0.0f, 0.0f, 1.0f, 0.0f}, //Vertex 2
-		Vertex{ 0.05f+shiftingx,-0.9f, 0.0f,              0.0f, 0.0f, 1.0f, 0.0f}, //Vertex 3
-		Vertex{ 0.05f+shiftingx, 0.0f+shiftingy, 0.0f,    0.0f, 0.0f, 1.0f, 0.0f}, //Vertex 4
-	};
-  */
-
-
-
 
   int numberofvertices = vertices.size();
 
  cout << "SIZE: "<< numberofvertices << endl;
 
 VertexBuffer vertixBuffer(vertices.data(), numberofvertices);
+
 
 	Shader shader("opengl/shader/basic.vs", "opengl/shader/basic.fs");
 	shader.bind();
@@ -98,13 +97,14 @@ VertexBuffer vertixBuffer(vertices.data(), numberofvertices);
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		vertixBuffer.bind();
-		glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
-    glDrawArrays(GL_TRIANGLE_STRIP, 4, 10);
-//    glDrawArrays(GL_TRIANGLE_STRIP, 12, 16);
+
+    for(int i = 0; i<p_count; i++){
+      glDrawArrays(GL_TRIANGLE_STRIP, i*4, 4);
+    }
+
 
 
 		SDL_GL_SwapWindow(window);
-
 
 
 		SDL_Event event;
